91b5307574c0c01e2b628829cfe32186067c1e5c
Manuella-Fernandes <manuellafernandes0240@gmail.com>
2026-05-04 16:16:32 -0400

    extracting the sentences

--- index.py
+++ index.py
@@ -4,4 +4,8 @@ from bs4 import BeautifulSoup
 pagina = requests.get('https://quotes.toscrape.com/')
 dados_pagina = BeautifulSoup(pagina.text, 'html.parser')
 
-print(dados_pagina.prettify())
+todas_frases = dados_pagina.find_all('div', class_="quote")
+
+for div in todas_frases:
+    texto = div.find('span', class_="text").text
+    print(texto)
